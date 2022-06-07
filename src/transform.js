75db470108612f4e9c89d2bb48fc1bd12001de9b
Fran Aliss <franalissar@gmail.com>
2022-06-07 11:39:04 -0400

    <Ciclo TDD> <Caso 3 al 9>

--- src/transform.js
+++ src/transform.js
@@ -1,12 +1,33 @@
 function transform(num) {
   let res = '';
-  if(num==1)
-  {
-    res = "uno";
-  }
-  if(num==2)
-  {
-    res = "dos";
+  switch(num){
+    case 1:
+      res = "uno";
+    break;
+    case 2:
+      res = "dos";
+    break;
+    case 3:
+      res = "tres";
+    break;
+    case 4:
+      res = "cuatro";
+    break;
+    case 5:
+      res = "cinco";
+    break;
+    case 6:
+      res = "seis";
+    break;
+    case 7:
+      res = "siete";
+    break;
+    case 8:
+      res = "ocho";
+    break;
+    case 9:
+      res = "nueve";
+    break;
   }
   return res;
 }
