71a7b1ee3b22f40ce628c0c712ec71708680490a
Fran Aliss <franalissar@gmail.com>
2022-06-07 11:46:30 -0400

    <Ciclo TDD> <Tests implementados desde num  3 al num 9>

--- src/transformador.test.js
+++ src/transformador.test.js
@@ -19,5 +19,17 @@ describe("Transformar", () => {
   it("Devolver cinco si se ingresa 5", () => {
     expect(transform(5)).toEqual("cinco");
   });
+  it("Devolver seis si se ingresa 6", () => {
+    expect(transform(6)).toEqual("seis");
+  });
+  it("Devolver siete si se ingresa 7", () => {
+    expect(transform(7)).toEqual("siete");
+  });
+  it("Devolver ocho si se ingresa 8", () => {
+    expect(transform(8)).toEqual("ocho");
+  });
+  it("Devolver nueve si se ingresa 9", () => {
+    expect(transform(9)).toEqual("nueve");
+  });
   
 });
